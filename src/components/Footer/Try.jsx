48bdda2a573206fbe1a96380a54a6b985bae7fe6
SalehinBinIqbal <salehin.rawna@gmail.com>
2023-08-13 22:33:05 +0600

    Added Footer

---
+++ src/components/Footer/Try.jsx
@@ -0,0 +1,116 @@
+import classes from './Try.module.css';
+
+function Try(){
+    return(
+        <div className={classes.body}>
+            <div className={classes.colorBox}>
+                <div className={classes.colorItems}>
+                    <div className={classes.blue}>
+                        
+                    </div>
+                    <div className={classes.red}>
+                        
+                    </div>
+                </div>
+                <div className={classes.colorItems}>
+                    <div className={classes.green}>
+                        
+                    </div>
+                    <div className={classes.blue}>
+                        
+                    </div>
+                </div>
+                <div className={classes.colorItems}>
+                    <div className={classes.red}>
+                        
+                    </div>
+                    <div className={classes.green}>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div className={classes.gridSection}>
+                <div className={classes.gridItems}>
+                    <div className={classes.gridBox}>
+                        A
+                    </div>
+                    <div className={classes.gridBox}>
+                        B
+                    </div>
+                    <div className={classes.gridBox}>
+                        C
+                    </div>
+                    <div className={classes.gridBox}>
+                        D
+                    </div>
+                    <div className={classes.gridBox}>
+                        E
+                    </div>
+                    <div className={classes.gridBox}>
+                        F
+                    </div>
+                </div>
+            </div>
+
+            <div className={classes.gridSectionLarge}>
+                <div className={classes.gridItemsLarge}>
+                    <div className={classes.gridBox2}>
+                        A
+                    </div>
+                    <div className={classes.gridBox2}>
+                        B
+                    </div>
+                    <div className={classes.gridBox2}>
+                        C
+                    </div>
+                    <div className={classes.gridBox2}>
+                        D
+                    </div>
+                    <div className={classes.gridBox2}>
+                        E
+                    </div>
+                    <div className={classes.gridBox2}>
+                        F
+                    </div>
+                    <div className={classes.gridBox2}>
+                        G
+                    </div>
+                    <div className={classes.gridBox2}>
+                        H
+                    </div>
+                    <div className={classes.gridBox2}>
+                        I
+                    </div>
+                </div>
+            </div>
+
+            <div className={classes.flexBox}>
+                <div className={classes.flexItems}>
+                    <div className={classes.item}>
+                        
+                    </div>
+                </div>
+                <div className={classes.flexItems}>
+                    <div className={classes.item}>
+                        
+                    </div>
+                    <div className={classes.item}>
+                        
+                    </div>
+                </div>
+                <div className={classes.flexItems}>
+                    <div className={classes.item}>
+                        
+                    </div>
+                    <div className={classes.item}>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Try;
