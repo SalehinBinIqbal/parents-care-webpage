c1fe0cceaeecc069c761654199e3e2763b4a19e3
SalehinBinIqbal <salehin.rawna@gmail.com>
2023-08-13 22:24:58 +0600

    Added Body section

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,16 @@
+import About from './About/About'
 import Card from './Card/Card'
 import Card2 from './Card2/Card2'
+import Objective from './Objective/Objective';
 import classes from './Body.module.css';
 
 function Body(){
     return(
         <div className={classes.bodyContainer}>
-            <Card />
+            <About/>
+            {/* <Card />
             <Card2/>
+            <Objective/> */}
         </div>
     );
 }
