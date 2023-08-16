dfb36bd72f4c66777e6187062a80920c2c102e3c
SalehinBinIqbal <salehin.rawna@gmail.com>
2023-08-16 18:08:19 +0600

    Updated Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,20 @@
-import photo from '../../assets/hero.jpg';
-import classes from './Hero.module.css';
+import hero from "../../assets/hero.jpg";
+import classes from "./Hero.module.css";
 
-function Hero(){
-    return(
-        <div className={classes.carouselPhoto}>
-            <img src={photo} alt="" />
-            <div className={classes.carouselText}>
-                <p className={classes.bigText}>Comprehensive Healthcare Solution for YOUR PARENTS!</p>
-                <p className={classes.smallText}>Plans starting at 1499 tk per month only</p>
-            </div>
-        </div>
-    );
+function Hero() {
+  return (
+    <div className={classes.heroSection}>
+      <img src={hero} alt="" />
+      <div className={classes.heroText}>
+        <p className={classes.heroTitle}>
+          Comprehensive Healthcare Solution for YOUR PARENTS!
+        </p>
+        <p className={classes.herodescription}>
+          Plans starting at 1499 tk per month only
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
