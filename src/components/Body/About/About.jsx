ab93ce69ddff5696a40f1c07693eebccb66c2f22
SalehinBinIqbal <salehin.rawna@gmail.com>
2023-08-16 18:06:50 +0600

    Updated About Section

--- src/components/Body/About/About.jsx
+++ src/components/Body/About/About.jsx
@@ -1,17 +1,17 @@
-import photo2 from "../../../assets/cardimage1.png";
-import photo1 from "../../../assets/cardimage2.webp";
-import photo3 from "../../../assets/nurse-holding-senior-man-s-hands-comfort.jpg";
-import photo4 from "../../../assets/objImage.jpg";
+import topRightPhoto from "../../../assets/cardimage1.png";
+import topLeftPhoto from "../../../assets/cardimage2.webp";
+import bottomLeftPhoto from "../../../assets/nurse-holding-senior-man-s-hands-comfort.jpg";
+import bottomRightPhoto from "../../../assets/objImage.jpg";
 import classes from "./About.module.css";
 
 function About() {
   return (
-    <div className={classes.aboutSectionContainer}>
-      <div className={classes.lightColorContainer}>
+    <div className={classes.aboutSection}>
+      <div className={classes.lightContainer}>
         <div className={classes.textArea}>
-          <p>About Parent Care Plan</p>
-          <p>What is Parent Care Plan (PCP)?</p>
-          <p>
+          <p className={classes.title}>About Parent Care Plan</p>
+          <p className={classes.boldTitle}>What is Parent Care Plan (PCP)?</p>
+          <p className={classes.description}>
             Parent care plan (PCP) is a comprehensive health plan for the aged
             parents or senior citizens which covers the primary & essential
             health care service needs! This plan covers the services starting
@@ -20,23 +20,31 @@ function About() {
         </div>
         <div className={classes.photoArea}>
           <div className={classes.stackedImage}>
-            <img src={photo1} alt="" />
+            <img className={classes.topLeftImage} src={topLeftPhoto} alt="" />
           </div>
           <div className={classes.stackedImage}>
-            <img src={photo2} alt="" />
+            <img className={classes.topRightImage} src={topRightPhoto} alt="" />
           </div>
           <div className={classes.stackedImage}>
-            <img src={photo3} alt="" />
+            <img
+              className={classes.bottomLeftImage}
+              src={bottomLeftPhoto}
+              alt=""
+            />
           </div>
           <div className={classes.stackedImage}>
-            <img src={photo4} alt="" />
+            <img
+              className={classes.bottomRightImage}
+              src={bottomRightPhoto}
+              alt=""
+            />
           </div>
         </div>
       </div>
-      <div className={classes.deepColorContainer}>
+      <div className={classes.deepContainer}>
         <div className={classes.photoArea2}>
           <div className={classes.borderImage}>
-            <img src={photo1} alt="" />
+            <img src={topLeftPhoto} alt="" />
           </div>
         </div>
         <div className={classes.textArea2}>
